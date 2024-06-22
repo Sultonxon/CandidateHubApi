d8dbc9dea09e6e852bf2e9d809edf83d4f6f42e7
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2024-06-22 16:02:56 +0500

    Create Candidate endpoint

---
+++ CandidateHub.Api/V1/Candidates/Controllers/CandidateController.cs
@@ -0,0 +1,20 @@
+using CandidateHub.Api.Commons.Exceptions;
+using CandidateHub.Api.V1.Candidates.Models;
+using CandidateHub.Api.V1.Candidates.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandidateHub.Api.V1.Candidates.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+public class CandidateController(ICandidateService candidateService) : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType<CandidateCreateOrUpdateModel>(200)]
+    public async Task<IActionResult> CreateOrUpdate([FromBody] CandidateCreateOrUpdateModel model)
+    {
+        var candidate = await candidateService.CreateOrUpdate(model);
+        return Ok(candidate);
+    }
+}
