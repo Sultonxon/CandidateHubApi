a3cb008cd374fc60a9a7fe977b846516f556aa62
Sultonxon <91827090+Sultonxon@users.noreply.github.com>
2024-06-22 16:46:54 +0500

    create paged result model

---
+++ CandidateHub.Api/Commons/Models/PagedResult.cs
@@ -0,0 +1,18 @@
+namespace CandidateHub.Api.Commons.Models;
+
+public class PagedResult<T>
+{
+    private List<T> Items { get; set; }
+    public int TotalItems { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => TotalItems % PageSize == 0 ? TotalItems / PageSize : TotalItems / PageSize + 1;
+    
+    public PagedResult(List<T> items, int totalItems, int page, int pageSize)
+    {
+        Items = items;
+        TotalItems = totalItems;
+        Page = page;
+        PageSize = pageSize;
+    }
+}
